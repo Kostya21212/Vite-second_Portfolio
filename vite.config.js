cc8b6b758e62353e8aea0e2beba1e54d8ed202b0
onischenko kostyantyn <onischenko411@gmail.com>
2024-03-03 13:16:26 +0200

    add index.html in vite .config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   build:{
     rollupOptions:{
       input:{
+        main: resolve(__dirname,'/index.html')
         about: resolve(__dirname,'./pages/about.html')
       }
     }
