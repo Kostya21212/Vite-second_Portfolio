4f47d93d0d34f366d645a2f86711d68260944f05
onischenko kostyantyn <onischenko411@gmail.com>
2024-03-03 12:20:00 +0200

    first files

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+document.querySelector('#app').innerHTML = /*html*/ `
+<h1 class="text-1xl font-bold underline">
+Hello world!
+</h1>
+`
+
