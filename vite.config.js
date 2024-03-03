bb02f28c1f0467454de342535b12a62920e3ecd6
onischenko kostyantyn <onischenko411@gmail.com>
2024-03-03 13:13:40 +0200

    add scss

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
-
+import {resolve} from 'path'
 export default defineConfig({
-  base: "/Vite-second_Portfolio/"
+  base: "/Vite-second_Portfolio/",
+  build:{
+    rollupOptions:{
+      input:{
+        about: resolve(__dirname,'./pages/about.html')
+      }
+    }
+  }
 })
